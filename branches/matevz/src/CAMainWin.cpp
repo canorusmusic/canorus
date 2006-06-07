afacc5e0ad611869362c89785bf704653b5577da
whiteangel <whiteangel@54f5b600-470d-0410-b7b0-af41ad535273>
2006-06-07 15:30:06 +0000

    Created my own branch for testing the KD trees and feta font usability. Will commit to trunk, if found out to be useful when done.

---
+++ branches/matevz/src/CAMainWin.cpp
@@ -0,0 +1,34 @@
+/*****************************************************************************/
+/*									     */
+/* This program is free software; you can redistribute it and/or modify it   */
+/* under the terms of the GNU General Public License as published by the     */ 
+/* Free Software Foundation; version 2 of the License.	                     */
+/*									     */
+/* This program is distributed in the hope that it will be useful, but       */
+/* WITHOUT ANY WARRANTY; without even the implied warranty of                */ 
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General  */
+/* Public License for more details.                                          */
+/*									     */
+/* You should have received a copy of the GNU General Public License along   */
+/* with this program; (See "LICENSE.GPL"). If not, write to the Free         */
+/* Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA        */
+/* 02111-1307, USA.				                             */
+/*									     */
+/*---------------------------------------------------------------------------*/
+/*									     */
+/*		Reinhard Katzmann, GERMANY			             */
+/*		reinhard@suamor.de					     */
+/*									     */
+/*									     */
+/*****************************************************************************/
+
+#include <QtGui/QtGui>
+
+#include "CAMainWin.h"
+
+// Constructor
+CAMainWin::CAMainWin(QMainWindow *oParent)
+  : QMainWindow( oParent )
+{
+  oMainWin.setupUi( this );
+}
