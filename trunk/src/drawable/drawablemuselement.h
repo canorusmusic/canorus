76f77d1b62798753332bf93f36c50772f82a8bba
suamor <suamor@54f5b600-470d-0410-b7b0-af41ad535273>
2006-07-29 21:17:04 +0000

    New directory hierarchy

---
+++ trunk/src/drawable/drawablemuselement.h
@@ -0,0 +1,37 @@
+/** @file drawablemuselement.h
+ * 
+ * Copyright (c) 2006, Matevž Jekovec, Canorus development team
+ * All Rights Reserved. See AUTHORS for a complete list of authors.
+ * 
+ * Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
+ */
+
+#ifndef DRAWABLEMUSELEMENT_H_
+#define DRAWABLEMUSELEMENT_H_
+
+#include "drawable.h"
+
+class CAMusElement;
+class CADrawableContext;
+
+class CADrawableMusElement : public CADrawable {
+	public:
+		CADrawableMusElement(CAMusElement *musElement, CADrawableContext *drawableContext, int x, int y);
+
+		enum CADrawableMusElementType {
+			DrawableNote,
+			DrawableClef
+		};
+				
+		inline CAMusElement *musElement() { return _musElement; }
+		CADrawableMusElementType drawableMusElementType() { return _drawableMusElement; }
+		CADrawableContext *drawableContext() { return _drawableContext; }
+		
+	protected:
+		CADrawableMusElementType _drawableMusElement;	///CADrawableMusElement Type
+
+		CADrawableContext *_drawableContext;
+		CAMusElement *_musElement;
+};
+
+#endif /*DRAWABLEMUSELEMENT_H_*/
---
+++ trunk/src/drawable/drawablenote.cpp
@@ -0,0 +1,141 @@
+/** @file drawablenote.cpp
+ * 
+ * Copyright (c) 2006, Matevž Jekovec, Canorus development team
+ * All Rights Reserved. See AUTHORS for a complete list of authors.
+ * 
+ * Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
+ */
+
+#include <QPainter>
+#include <iostream>
+#include "drawablenote.h"
+#include "voice.h"
+#include "staff.h"
+#include "note.h"
+
+#define QUARTER_STEM_LENGTH 22
+#define QUARTER_YPOS_DELTA 21
+
+CADrawableNote::CADrawableNote(CANote *note, CADrawableContext *drawableContext, int x, int y, bool shadowNote)
+ : CADrawableMusElement(note, drawableContext, x, y) {
+	_drawableMusElement = CADrawableMusElement::DrawableNote;
+	
+	switch (note->noteLength()) {
+		case CANote::Quarter:
+			_width = 11;
+			_height = 26;
+			_yPos = y - QUARTER_YPOS_DELTA;	//values in constructor are the notehead center coords. yPos represents the top of the stem.
+			_xPos = x - _width/2;
+			_neededWidth = _width;
+			_neededHeight = _height;
+			break;
+			
+		case CANote::Whole:
+			_width = 16;
+			_height = 3;
+			_yPos = y - _height/2;
+			_xPos = x - _width/2;
+			_neededWidth = _width;
+			_neededHeight = _height;
+			break;			
+	}
+	
+	_shadowNote = shadowNote;
+	
+	_drawLedgerLines = true;
+}
+
+CADrawableNote::~CADrawableNote() {
+}
+
+void CADrawableNote::draw(QPainter *p, CADrawSettings s) {
+	QFont font("Emmentaler");
+	font.setPixelSize((int)(35*s.z));
+	
+	p->setPen(QPen(s.color));
+	p->setFont(font);
+
+	QPen pen;
+	switch (note()->noteLength()) {
+		case CANote::Quarter:
+			//draw notehead
+			s.y += (int)(QUARTER_STEM_LENGTH*s.z);
+			p->drawText(s.x,(int)(s.y - 0.1*s.z),QString(0xE125));
+
+			//draw stem
+			pen.setWidth((int)(1.2*s.z));
+			pen.setCapStyle(Qt::RoundCap);
+			pen.setColor(s.color);
+			p->setPen(pen);
+			p->drawLine((int)(s.x + (_width - 0.2)*s.z), (int)(s.y-1*s.z), (int)(s.x + (_width - 0.2)*s.z), s.y-(int)(QUARTER_STEM_LENGTH*s.z));
+						
+			break;
+			
+		case CANote::Whole:
+			//draw notehead
+			s.y += (int)((_height*s.z)/2);
+			p->drawText(s.x, (int)(s.y), QString(0xE123));
+
+			break;
+	}
+	
+	//draw ledger lines
+	if ( _drawLedgerLines &&
+	     note() && note()->voice() && note()->voice()->staff() &&
+	     ( (note()->notePosition() <= -2) ||	//note is below the staff
+	       (note()->notePosition() >= note()->voice()->staff()->numberOfLines()*2)	//note is above the staff
+	     )
+	   ) {
+	   	int direction = (note()->notePosition() > 0 ? 1 : -1);	//1 falling, -1 rising
+	   	int ledgerDist = (int)(9.0*s.z);	//distance between the ledger lines - notehead height
+	   	
+	   	//draw ledger lines in direction from the notehead to staff
+	   	int y = (int)(s.y-0.6*s.z);	//initial y
+	   	if ((note()->notePosition() % 2) != 0)	//if the note is not on the ledger line, shift the ledger line by half space
+	   		y += (ledgerDist/2)*direction;
+
+		QPen pen(s.color);
+		pen.setWidth((int)(1.0*s.z));
+		p->setPen(pen);
+	   	for (int i=0;
+	   	     i < ((note()->notePosition()*direction -
+	   	          ((direction>0)?((note()->voice()->staff()->numberOfLines()-1)*2):0))/2);
+	   	     ++i, y += (ledgerDist*direction)
+	   	    )
+			p->drawLine((int)(s.x - 4*s.z), (int)(y), (int)(s.x + (_width + 4)*s.z), (int)(y));
+	}
+	
+}
+
+CADrawableNote *CADrawableNote::clone() {
+	switch (note()->noteLength()) {
+		case CANote::Quarter:
+			return new CADrawableNote(note(), drawableContext(), _xPos + _width/2, _yPos + QUARTER_YPOS_DELTA);
+			break;
+		case CANote::Whole:
+			return new CADrawableNote(note(), drawableContext(), _xPos + _width/2, _yPos + _height/2);
+			break;		
+	}
+}
+
+void CADrawableNote::setXPos(int xPos) {
+	switch (note()->noteLength()) {
+		case CANote::Quarter:
+			_xPos = xPos - _width/2;
+			break;
+		case CANote::Whole:
+			_xPos = xPos - _width/2;
+			break;
+	}
+}
+
+void CADrawableNote::setYPos(int yPos) {
+	switch (note()->noteLength()) {
+		case CANote::Quarter:
+			_yPos = yPos - QUARTER_YPOS_DELTA;
+			break;
+		case CANote::Whole:
+			_yPos = yPos - _height/2;
+			break;
+	}
+}
