750dd0dca50d57377db290b719e97d97b939530f
whiteangel <whiteangel@54f5b600-470d-0410-b7b0-af41ad535273>
2006-11-02 20:57:06 +0000

    Improved function names and their keys engraving. It now uses less space and are better fit.

--- trunk/src/drawable/drawablefunctionmarking.h
+++ trunk/src/drawable/drawablefunctionmarking.h
@@ -31,6 +31,7 @@ class CADrawableFunctionMarking : public CADrawableMusElement {
 	private:
 		bool _extenderLineVisible;		///Should the function draw a horizontal line until the end of the function
 		int _fontWidth;			///Width of the font itself - needed to determine where to start the extender line
+		QString _text;			///Function transformed to String which is rendered then
 };
 
 /**
