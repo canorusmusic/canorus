da5ee515a58ec0b6b1ee5249e4f924fbcd43de16
whiteangel <whiteangel@54f5b600-470d-0410-b7b0-af41ad535273>
2006-12-13 10:05:42 +0000

    Fixed neutral stem position bug.

--- trunk/src/scripts/newdocument.py
+++ trunk/src/scripts/newdocument.py
@@ -6,8 +6,8 @@ def newDefaultDocument(document):
 	staff1 = _CanorusPython.Sheet_addStaff(sheet1)
 	staff1voice1 = _CanorusPython.new_Voice(staff1, 'Staff1Voice1')
 	_CanorusPython.Staff_addVoice(staff1, staff1voice1)
-	staff1voice2 = _CanorusPython.new_Voice(staff1, 'Staff1Voice2')
-	_CanorusPython.Staff_addVoice(staff1, staff1voice2)
+#	staff1voice2 = _CanorusPython.new_Voice(staff1, 'Staff1Voice2')
+#	_CanorusPython.Staff_addVoice(staff1, staff1voice2)
 	_CanorusPython.Staff_insertSign(staff1, _CanorusPython.new_Clef(_CanorusPython.Clef_Treble, staff1, 0))
 	_CanorusPython.Staff_insertSign(staff1, _CanorusPython.new_TimeSignature(4, 4, staff1, 0))
 	
