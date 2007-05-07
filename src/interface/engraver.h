b4e6c9396f5afeb8043bdd272667fa7b3706aefc
Matevž Jekovec <83672+matevz@users.noreply.github.com>
2007-05-07 02:43:52 +0000

    Implemented multiple selection using drag and drop or SHIFT key. Added support for deletion of multiple elements. Fixed a crash when removing voice. Fixed a crash when changing the current voice.

--- src/interface/engraver.h
+++ src/interface/engraver.h
@@ -1,23 +1,18 @@
-/** @file interface/engraver.h
- * 
- * Copyright (c) 2006, Matevž Jekovec, Canorus development team
- * All Rights Reserved. See AUTHORS for a complete list of authors.
- * 
- * Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
- */
+/*!
+	Copyright (c) 2006, Matevž Jekovec, Canorus development team
+	All Rights Reserved. See AUTHORS for a complete list of authors.
+	
+	Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
+*/
 
 #ifndef ENGRAVER_
 #define ENGRAVER_
 
 class CAScoreViewPort;
 
-/**
- * This class is a bridge between the data part of Canorus and the UI.
- * Out of data CAMusElement* and CAContext* objects, it creates their CADrawable* instances.
- */
 class CAEngraver {
 	public:
 		static void reposit(CAScoreViewPort *v);
 };
 
-#endif /*ENGRAVER_*/
+#endif /* ENGRAVER_ */
