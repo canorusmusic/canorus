405d00f2bf9765569b5481fb5c2339a9d9650e81
Matevž Jekovec <83672+matevz@users.noreply.github.com>
2007-06-02 16:49:42 +0000

    Some cosmetic fixes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,9 @@
-/** \file main.cpp
- * 
- * Copyright (c) 2006-2007, Reinhard Katzmann, Matevž Jekovec, Canorus development team
- * All Rights Reserved. See AUTHORS for a complete list of authors.
- * 
- * Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
- */
+/*! 
+	Copyright (c) 2006-2007, Reinhard Katzmann, Matevž Jekovec, Canorus development team
+	All Rights Reserved. See AUTHORS for a complete list of authors.
+	
+	Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
+*/
 
 #include <QApplication>
 #include <QTranslator>
