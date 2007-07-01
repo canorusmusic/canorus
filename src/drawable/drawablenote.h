06ae2b856ec78687bb28359ce84166c24f0f6a52
Matevž Jekovec <83672+matevz@users.noreply.github.com>
2007-07-01 14:39:13 +0000

    Added lyrics support to scripting engine.

--- src/drawable/drawablenote.h
+++ src/drawable/drawablenote.h
@@ -1,10 +1,9 @@
-/** @file drawable/drawablenote.h
- * 
- * Copyright (c) 2006, Matevž Jekovec, Canorus development team
- * All Rights Reserved. See AUTHORS for a complete list of authors.
- * 
- * Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
- */
+/*!
+	Copyright (c) 2006, Matevž Jekovec, Canorus development team
+	All Rights Reserved. See AUTHORS for a complete list of authors.
+	
+	Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
+*/
 
 #ifndef DRAWABLENOTE_H_
 #define DRAWABLENOTE_H_
