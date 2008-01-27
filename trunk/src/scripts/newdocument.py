8fc2b37f543bff603dd4a1dd6fb2fbd34640016e
whiteangel <whiteangel@54f5b600-470d-0410-b7b0-af41ad535273>
2008-01-27 21:27:45 +0000

    Fixed newdocument script for new class names.

--- trunk/src/scripts/newdocument.py
+++ trunk/src/scripts/newdocument.py
@@ -4,14 +4,14 @@ def newDefaultDocument(document):
 	sheet1 = document.addSheetByName( tr('Sheet%1').replace('%1',str(1)) )
 	
 	staff1 = sheet1.addStaff()
-	staff1.addVoice( Voice( tr('Staff%1').replace('%1',str(1)) + tr('Voice%1').replace('%1',str(1)), staff1, Note.StemUp) )
-	staff1.addVoice( Voice( tr('Staff%1').replace('%1',str(1)) + tr('Voice%1').replace('%1',str(2)), staff1, Note.StemDown) )
+	staff1.addVoice( CAVoice( tr('Staff%1').replace('%1',str(1)) + tr('Voice%1').replace('%1',str(1)), staff1, CANote.StemUp) )
+	staff1.addVoice( CAVoice( tr('Staff%1').replace('%1',str(1)) + tr('Voice%1').replace('%1',str(2)), staff1, CANote.StemDown) )
 	staff1.voiceAt(0).append( CAClef( CAClef.Treble, staff1, 0 ) )
 	staff1.voiceAt(0).append( CATimeSignature( 4, 4, staff1, 0 ) )
 	
 	staff2 = sheet1.addStaff()
-	staff2.addVoice( Voice( tr('Staff%1').replace('%1',str(2)) + tr('Voice%1').replace('%1',str(1)), staff2, Note.StemUp) )
-	staff2.addVoice( Voice( tr('Staff%1').replace('%1',str(2)) + tr('Voice%1').replace('%1',str(2)), staff2, Note.StemDown) )
+	staff2.addVoice( CAVoice( tr('Staff%1').replace('%1',str(2)) + tr('Voice%1').replace('%1',str(1)), staff2, CANote.StemUp) )
+	staff2.addVoice( CAVoice( tr('Staff%1').replace('%1',str(2)) + tr('Voice%1').replace('%1',str(2)), staff2, CANote.StemDown) )
 	staff2.voiceAt(0).append( CAClef( CAClef.Bass, staff2, 0 ) )
 	staff2.voiceAt(0).append( CATimeSignature( 4, 4, staff2, 0 ) )
 	
