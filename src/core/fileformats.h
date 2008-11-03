627ca68f0ff5c4da70f58403c7b23478ee1d977a
Reinhard <567396+suamor@users.noreply.github.com>
2008-11-03 13:44:58 +0000

    Add PDF and SVG support

--- src/core/fileformats.h
+++ src/core/fileformats.h
@@ -1,5 +1,5 @@
 /*! 
-	Copyright (c) 2006-2007, Matevž Jekovec, Canorus development team
+	Copyright (c) 2006-2008, Reinhard Katzmann, Matevž Jekovec, Canorus development team
 	All Rights Reserved. See AUTHORS for a complete list of authors.
 	
 	Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
@@ -25,7 +25,9 @@ public:
 		Noteworthy = 9,
 		Igor       = 10,
 		Capella    = 11,
-		Midi       = 12
+		Midi       = 12,
+		PDF        = 13,
+    SVG        = 14
 	};
 	
 	static const QString LILYPOND_FILTER;
@@ -38,6 +40,8 @@ public:
 	static const QString SIBELIUS_FILTER;
 	static const QString CAPELLA_FILTER;
 	static const QString MIDI_FILTER;
+	static const QString PDF_FILTER;
+	static const QString SVG_FILTER;
 	
 	static const QString getFilter( const CAFileFormatType );
 	static const CAFileFormatType getType( const QString );
