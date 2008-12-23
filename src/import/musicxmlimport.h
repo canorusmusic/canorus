d373a0e396de233c774d80047de1fd60f4895ac4
Matevž Jekovec <83672+matevz@users.noreply.github.com>
2008-12-23 18:52:23 +0000

    Forget to upload MusicXML stuff.

--- src/import/musicxmlimport.h
+++ src/import/musicxmlimport.h
@@ -8,7 +8,7 @@
 #ifndef MUSICXMLIMPORT_H_
 #define MUSICXMLIMPORT_H_
 
-#include <QXmlDefaultHandler>
+#include <QXmlStreamReader>
 #include <QString>
 #include <QStack>
 #include <QHash>
@@ -19,69 +19,42 @@
 #include "import/import.h"
 
 class CADocument;
-class QDomNode;
 class CASheet;
 class CAStaff;
 class CAKeySignature;
 class CATimeSignature;
 
-class CAMusicXmlImport: public CAImport, public QXmlDefaultHandler {
+class CAMusicXmlImport: public CAImport, private QXmlStreamReader {
 public:
-	enum CAMusicXmlType {
-		TimeWise,
-		PartWise,
-		Undefined
-	};
-
 	CAMusicXmlImport( QTextStream *stream=0 );
 	CAMusicXmlImport( const QString stream );
 	virtual ~CAMusicXmlImport();
 
-	bool startElement(const QString& namespaceURI, const QString& localName, const QString& qName, const QXmlAttributes& attributes);
-	bool endElement(const QString& namespaceURI, const QString& localName, const QString& qName);
-	bool fatalError(const QXmlParseException& exception);
-	bool characters(const QString& ch);
+	const QString readableStatus();
 
 private:
 	void initMusicXmlImport();
 	CADocument* importDocumentImpl();
 
-	bool parseDocumentStartElement(const QString& namespaceURI, const QString& localName, const QString& qName, const QXmlAttributes& attributes);
-	bool parseDocumentEndElement(const QString& namespaceURI, const QString& localName, const QString& qName);
-	bool parsePartListStartElement(const QString& namespaceURI, const QString& localName, const QString& qName, const QXmlAttributes& attributes);
-	bool parsePartListEndElement(const QString& namespaceURI, const QString& localName, const QString& qName);
-	bool parsePartStartElement(const QString& namespaceURI, const QString& localName, const QString& qName, const QXmlAttributes& attributes);
-	bool parsePartEndElement(const QString& namespaceURI, const QString& localName, const QString& qName);
+	void readHeader();
+	void readScorePartwise();
+	void readScoreTimewise();
+	void readWork();
+	void readIdentification();
+	void readDefaults();
+	void readPartList();
+	void readPart();
+	void readMeasure( CAStaff* );
+	void readAttributes( CAStaff* );
+	void readNote( CAStaff*, int );
 
-	CAMusicXmlType  _type;
-	QString         _version;
-	QStack<QString> _depth;
-	QString         _cha;
-	QString         _errorMsg;
+	QString         _musicXmlVersion;
 
 	CADocument *_document;
-	CASheet    *_curSheet;
-	CAStaff    *_curStaff;
 	QHash<QString, CAStaff*> _staffMap;
 	QHash<CAStaff*, int> _midiChannel;
 	QHash<CAStaff*, int> _midiProgram;
-	QMultiHash<CAStaff*, CAVoice* > _voiceMap;
-
-	// Part attributes
-	QMultiHash<CAStaff*, int> _divisions;
-	QMultiHash<CAStaff*, CAKeySignature*> _keySignature;
-	QMultiHash<CAStaff*, CATimeSignature*> _timeSignature;
-
-
-	// Notes and rests
-	CAPlayableLength::CAMusicLength _playableLength;
-	bool _playableRest;
-	int _playableVoiceNumber;
-	CADiatonicPitch _notePitch;
-
-
-
-	QString _creatorType;
+	QHash<CAStaff*, int> _divisions;
 };
 
 #endif /* MUSICXMLIMPORT_H_ */
