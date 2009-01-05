3c85feb94bd15e28846682720eee1b1865c6e0a0
Itay Perl <614418+itayperl@users.noreply.github.com>
2009-01-05 20:49:50 +0000

    Fixed uneven staff line spacing. Fixed position of notes and ledger lines.

--- src/drawable/drawablenote.h
+++ src/drawable/drawablenote.h
@@ -1,5 +1,5 @@
 /*!
-	Copyright (c) 2006, Matevž Jekovec, Canorus development team
+	Copyright (c) 2006-2009, Matevž Jekovec, Canorus development team
 	All Rights Reserved. See AUTHORS for a complete list of authors.
 	
 	Licensed under the GNU GENERAL PUBLIC LICENSE. See COPYING for details.
@@ -39,7 +39,12 @@ class CADrawableNote : public CADrawableMusElement {
 		bool _shadowNote;	///Is the current note shadow note?
 		CADrawableAccidental *_drawableAcc;
 		CANote::CAStemDirection _stemDirection;	/// This value is StemUp or StemDown only, no StemPreferred or StemNeutral present. We generate this on CADrawableNote constructor.
+		int _stemLength;
 		int _noteHeadWidth;
+		float _penWidth; // pen width for stem
+		int _noteHeadCodepoint; // Unicode codepoint for the notehead symbol.
+		int _stemUpCodepoint;
+		int _stemDownCodepoint;
 		static const int HUNDREDTWENTYEIGHTH_STEM_LENGTH;
 		static const int SIXTYFOURTH_STEM_LENGTH;
 		static const int THIRTYSECOND_STEM_LENGTH;
