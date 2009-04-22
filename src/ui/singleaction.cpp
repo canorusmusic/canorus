15421ec92aaad9b0bbc1990e4c39a42b604f8106
Reinhard <567396+suamor@users.noreply.github.com>
2009-04-22 18:52:12 +0000

    CASingleAction class contains information about one control command

---
+++ src/ui/singleaction.cpp
@@ -0,0 +1,59 @@
+/*!
+	Copyright (c) 2009, Reinhard Katzmann, Matevž Jekovec, Canorus development team
+	All Rights Reserved. See AUTHORS for a complete list of authors.
+
+	Licensed under the GNU GENERAL PUBLIC LICENSE. See LICENSE.GPL for details.
+*/
+
+#include <QString>
+
+#include "singleaction.h"
+
+
+CASingleAction::CASingleAction( QObject *parent )
+: QAction( parent )
+{
+}
+
+CASingleAction:~CASingleAction()
+{
+}
+
+void CASingleAction::setCommand( QString oCommand )
+{
+	if( !oCommand.isEmpty() )
+	{
+		_oCommand = oCommand;
+		setText( oCommand );
+	}
+}
+
+void CASingleAction::setContext( QString oContext )
+{
+	if( !oContext.isEmpty() )
+		_oContext = oContext;
+}
+
+void CASingleAction::setDescription( QString oDescription )
+{
+	if( !oDescription.isEmpty() )
+	{
+		_oDescription = oDescription;
+		setToolTip( oDescription );
+	}
+}
+
+void CASingleAction::setShortCut( QString oShortCut )
+{
+	if( !oShortCut.isEmpty() )
+	{
+		_oShortCut = oShortCut;
+		QAction::setShortCut( oShortCut );
+	}
+}
+
+void CASingleAction::setMidiCommand( QString oMidiCommand )
+{
+	if( !oMidiCommand.isEmpty() )
+		_oMidiCommand = oMidiCommand;
+}
---
+++ src/ui/singleaction.h
@@ -0,0 +1,46 @@
+/*!
+	Copyright (c) 2009, Reinhard Katzmann, Matevž Jekovec, Canorus development team
+	All Rights Reserved. See AUTHORS for a complete list of authors.
+
+	Licensed under the GNU GENERAL PUBLIC LICENSE. See LICENSE.GPL for details.
+*/
+
+#ifndef _CASINGLEACTION_H_
+#define _CASINGLEACTION_H_
+
+#include <QAction>
+
+class QString;
+
+// One action based on QAction, Midi and QString. Contains all
+// information (including description) for one control command
+class CASingleAction : public QAction
+{
+public:
+	// Constructor / Desctructor
+	CASingleAction( QObject * );
+	~CASingleAction();
+
+	// Getter methods for all single action parameters
+	inline QString getCommand()     { return _oCommand; }
+	inline QString getContext()     { return _oContext; }
+	inline QString getDescription() { return _oDescription; }
+	inline QString getShortCut()    { return _oShortCut; }
+	inline QString getMidiCommand() { return _oMidiCommand; }
+	
+	// Setter methods
+	void setCommand( QString oCommand );
+	void setContext( QString oContext );
+	void setDescription( QString oDescription );
+	void setShortCut( QString oShortCut );
+	void setMidiCommand( QString oMidiCommand );
+
+protected:
+	QString _oCommand;
+	QString _oContext;
+	QString _oDescription;
+	QString _oShortCut;
+	QString _oMidiCommand;
+}
+
+#endif // _CASINGLEACTION_H_
