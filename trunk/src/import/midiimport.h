b3525bede9ffe8d25fd4e8d20dd1dd7c87636634
itsgeorg <itsgeorg@54f5b600-470d-0410-b7b0-af41ad535273>
2009-06-13 08:19:54 +0000

    building chords works almost, still disabled

--- trunk/src/import/midiimport.h
+++ trunk/src/import/midiimport.h
@@ -126,6 +126,7 @@ private:
 	CAMusElement* getOrCreateTimeSignature( int time, int channel, int voiceIndex, CAStaff *staff, CAVoice *voice );
 	int _numberOfAllVoices;
 	void fixAccidentals( CASheet *s );
+	void appendNoteToChord( QList<CAMidiImportEvent*> *events, int index, CAStaff *staff, CATimeSignature *ts );
 };
 
 #endif /* MIDIIMPORT_H_ */
