150246e9d6fb8fec6c3916ff4e00f04f2aced068
matevz <matevz@54f5b600-470d-0410-b7b0-af41ad535273>
2018-06-09 23:15:11 +0000

    Show bar length error, also for first partial bar, if it is too long.

--- trunk/src/core/notechecker.cpp
+++ trunk/src/core/notechecker.cpp
@@ -60,8 +60,10 @@ void CANoteChecker::checkSheet(CASheet *sheet) {
 				lastTimeSigRequiredDuration = static_cast<CATimeSignature*>(timeSigs[lastTimeSigIdx])->barDuration();
 			}
 			
-			// check the bar duration
-			if ((lastBarlineTime != -1) && (barlines[j]->timeStart()!=lastBarlineTime+lastTimeSigRequiredDuration)) {
+			// check the bar duration.
+			// If first bar is partial, the length should be shorter or equal to time sig.
+			if ((lastBarlineTime == -1 && barlines[j]->timeStart()>lastBarlineTime+lastTimeSigRequiredDuration) ||
+				(lastBarlineTime != -1 && barlines[j]->timeStart()!=lastBarlineTime+lastTimeSigRequiredDuration)) {
 				CANoteCheckerError *nce = new CANoteCheckerError(barlines[j], QObject::tr("Bar duration incorrect."));
 				sheet->addNoteCheckerError(nce);
 			}
