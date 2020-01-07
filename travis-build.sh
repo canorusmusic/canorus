42c93f2e00f1ac5307ad23cf7b782682294c6c13
Matevž Jekovec <matevz.jekovec@gmail.com>
2020-01-08 00:35:42 +0100

    add code styling mandatory

--- travis-build.sh
+++ travis-build.sh
@@ -1,8 +1,21 @@
-#!/bin/sh
-cd ..
-mkdir -p build
-cd build
+#!/bin/bash
+# This script is run from the CI service and does:
+# 1) checks, if code is correctly formatted.
+# 2) compiles canorus
+# 3) installs canorus locally
+
+set -euxo pipefail
+
+mkdir -p ../build
+cd ../build
 cmake -D CMAKE_INSTALL_PREFIX=~/.local ../canorus
+
+# Check, if code is formatted with "make fmt".
+make fmt
+cd ../canorus
+git diff-index --quiet HEAD
+cd ../build
+
 make -j4 
 make tr
 make install
